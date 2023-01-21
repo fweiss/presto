535df1a24be195da526cfa851a8a89bd77846e81
Frank E Weiss <feweiss@gmail.com>
2023-01-21 09:43:03 -0800

    Add effect while request in flight

--- html/main.js
+++ html/main.js
@@ -23,3 +23,26 @@ formElem.addEventListener('submit', (e) => {
       });
 
 })
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    responseElem.disabled = true
+    responseElem.classList.add('spinner')
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+})
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    responseElem.disabled = false
+    responseElem.classList.remove('spinner')
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+})
