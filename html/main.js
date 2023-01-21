773bbb78293e964373de699965c0206e30f68fd3
fweiss <feweiss@gmail.com>
2023-01-20 20:30:37 -0800

    Update preview iframe with modified markup

--- html/main.js
+++ html/main.js
@@ -2,17 +2,21 @@
 import axios from '/axios.js'
 
 const formElem = document.getElementById('request')
-const inputElem = document.getElementById('input')
+const promptElem = document.getElementById('prompt')
 const responseElem = document.getElementById('response')
+const previewElem = document.getElementById('preview')
 
 formElem.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const prompt = inputElem.value
+    const prompt = promptElem.value
     axios
       .post("http://localhost:8080/chat", { prompt })
       .then((res) => {
-        responseElem.innerText = res.data
+        responseElem.innerText = res.data.trim()
+
+        const iframeDocument = previewElem.contentDocument
+        iframeDocument.body.innerHTML = res.data
       })
       .catch((err) => {
         console.error(err);
